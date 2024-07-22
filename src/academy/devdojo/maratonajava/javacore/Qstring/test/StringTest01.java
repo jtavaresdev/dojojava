90fc14dd279202b898be4c304fe1930533e2aeff
Joao Tavares <jtavaresgestor@gmail.com>
2024-07-22 11:11:19 -0300

    classes

--- src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest01.java
@@ -2,8 +2,12 @@ package academy.devdojo.maratonajava.javacore.Qstring.test;
 
 public class StringTest01 {
     public static void main(String[] args) {
-        String nome = "Joao";
+        String nome = "Joao"; // String pool
         String nome2 = "Joao";
+        String nome3 = new String("Joao");// Heap - Cria um objeto com uma ref em memoria
+        
+
         System.out.println(nome2 == nome);
+        System.out.println(nome == nome3);
     }
 }
