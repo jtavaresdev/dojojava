56f86ba61bd94d9d253034357801f8ded9b76407
sunless <jtavaresgestor@gmail.com>
2024-07-22 09:53:06 -0300

    class

---
+++ src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest01.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.javacore.Qstring.test;
+
+public class StringTest01 {
+    public static void main(String[] args) {
+        String nome = "Joao";
+        String nome2 = "Joao";
+        System.out.println(nome2 == nome);
+    }
+}
