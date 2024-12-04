94d3d91eeaa6a910a9b7241224cb42dd13757493
João V. Tavares <jtavaresgestor@gmail.com>
2024-12-04 13:55:42 -0300

    oi

---
+++ src/academy/devdojo/maratonajava/javacore/Vio/test/FileTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.Vio.test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileTest01 {
+    public static void main(String[] args) {
+        File file = new File("/home/joao/Área de trabalho/projects/dojojava-master/arquivo/teste");
+        try {
+            boolean isCreated = file.createNewFile();
+            System.out.println(isCreated);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+}
